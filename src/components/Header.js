70ea034e3d57f2d66835b5c00bf4aa1a64aaf96d
michalosman <michal.osman.997@gmail.com>
2021-04-11 18:04:25 +0200

    Update title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <HeaderWrapper>
-      <Title>Memory card</Title>
+      <Logo src={logo} alt="logo"></Logo>
     </HeaderWrapper>
   )
 }
 
 const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: center;
   padding: 2rem;
   background-color: ${({ theme }) => theme.colors.dark};
   color: ${({ theme }) => theme.colors.light};
 `
 
-const Title = styled.h1`
-  font-weight: 800;
-  text-align: center;
+const Logo = styled.img`
+  max-width: 600px;
 `
 
 export default Header
